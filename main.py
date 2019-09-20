d84158eb4ce6de921e810c71ef4f81715d6ddb24
Sam Jones <sam.jones494@googlemail.com>
2019-09-20 15:21:58 +0100

    add more rigorus error handling for command line args

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import changer.changer as changer
 
 
 def main():
-    if len(sys.argv) < 2 or sys.argv[1].isdigit() is False:
-        print("Please provide a numeric argument")
+    if __invalid_args():
+        raise Exception('Please provide a numeric argument')
     else:
         print(changer.calculate(int(sys.argv[1])))
+
+
+def __invalid_args():
+    return len(sys.argv) < 2 or sys.argv[1].isdigit() is False
 
 
 if __name__ == '__main__': main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 import sys
+import pytest
 from context import main
+
 
 def test_correct_usage(capfd):
     out = launch(["main.py", "27"], capfd)
@@ -7,13 +9,13 @@ def test_correct_usage(capfd):
 
 
 def test_errors_if_no_arguments_passed(capfd):
-    out = launch(["main.py"], capfd)
-    assert out == "Please provide a numeric argument\n"
+    with pytest.raises(Exception):
+        launch(["main.py"], capfd)
 
 
 def test_errors_if_non_numeric_argument_passed(capfd):
-    out = launch(["main.py", "wrong argument type"], capfd)
-    assert out == "Please provide a numeric argument\n"
+    with pytest.raises(Exception):
+        launch(["main.py", "wrong argument type"], capfd)
 
 
 def launch(params, capfd):
