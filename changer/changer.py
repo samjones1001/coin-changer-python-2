736420dbb9885b253fea4e2201b5294acc0768be
Sam Jones <sam.jones494@googlemail.com>
2019-09-20 15:49:18 +0100

    add handler function for aws lambda

--- changer/changer.py
+++ changer/changer.py
@@ -1,3 +1,13 @@
+import json
+
+def handler(event, context):
+    change_amount = event['queryStringParameters']['change_amount']
+    response = {
+        'statusCode': 200,
+        'body': json.dumps(calculate(change_amount))
+    }
+    return response
+
 def calculate(change_amount, coins=None):
     if coins is None: coins = []
     for coin in (200, 100, 50, 20, 10, 5, 2, 1):
