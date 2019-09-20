f7628d446e1664daae8d0b9a6bf1f699e38ae84d
Sam Jones <sam.jones494@googlemail.com>
2019-09-20 14:51:38 +0100

    calculate function correctly handles amounts requiring three coins

--- changer/changer.py
+++ changer/changer.py
@@ -2,6 +2,7 @@ def calculate(change_amount, coins=None):
     if coins is None: coins = []
     for coin in (200, 100, 50, 20, 10, 5, 2, 1):
         if change_amount >= coin:
-            change_amount -= coin
-            coins.append(coin)
+            for _ in range(int(change_amount / coin)):
+                change_amount -= coin
+                coins.append(coin)
     return coins
