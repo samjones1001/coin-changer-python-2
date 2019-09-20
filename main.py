f3fe75f5a7c823fccf53cc7aa0dff22639b47926
Sam Jones <sam.jones494@googlemail.com>
2019-09-20 15:10:40 +0100

    throw error if passed no cl arguments

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import changer.changer as changer
 
 
 def main():
-    print(changer.calculate(int(sys.argv[1])))
+    if len(sys.argv) < 2:
+        print("Please provide an argument")
+    else:
+        print(changer.calculate(int(sys.argv[1])))
 
 
 if __name__ == '__main__': main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,10 @@ def test_correct_usage(capfd):
     out = launch(["main.py", "27"], capfd)
     assert out == "[20, 5, 2]\n"
 
+def test_errors_if_no_arguments_passed(capfd):
+    out = launch(["main.py"], capfd)
+    assert out == "Please provide an argument\n"
+
 def launch(params, capfd):
     sys.argv = params
     main()
