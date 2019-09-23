7572462f7410bef7b37a5586c32e7b03125c53ff
Sam Jones <sam.jones494@googlemail.com>
2019-09-23 13:49:45 +0100

    refactor test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,25 +1,51 @@
 import sys
 import pytest
-from context import cli_main
+from coin_changer import cli_main, lambda_main
 
 
-def test_correct_usage(capfd):
-    out = launch(["coin_changer.py", "27"], capfd)
+class Runner():
+    def __init__(self, capfd):
+        self._capfd = capfd
+
+    def run(self, *args):
+        sys.argv = ["coin_changer.py", *args]
+        cli_main()
+        out,err = self._capfd.readouterr()
+
+        if err is not None:
+            print(err)
+
+        return out
+
+@pytest.fixture
+def app(capfd):
+    return Runner(capfd)
+
+
+def test_correct_usage(app):
+    out = app.run("27")
     assert out == "[20, 5, 2]\n"
 
 
 def test_errors_if_no_arguments_passed(capfd):
     with pytest.raises(Exception):
-        launch(["coin_changer.py"], capfd)
+        app.run()
 
 
 def test_errors_if_non_numeric_argument_passed(capfd):
     with pytest.raises(Exception):
-        launch(["coin_changer.py", "wrong argument type"], capfd)
+        app.run("wrong argument type")
 
 
-def launch(params, capfd):
-    sys.argv = params
-    cli_main()
-    out, err = capfd.readouterr()
-    return out
+def can_be_run_from_lambda_entry_point():
+    event = {
+        'queryStringParameters': {
+            'change_amount': 25
+        }
+    }
+
+    expected_output = {
+        'statusCode': 200,
+        'body': '[20, 5]'
+    }
+    assert lambda_main(event, {}) == expected_output
