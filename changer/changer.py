f44a9af221bd5bc4e7607978c724a3b8708e6eff
Sam Jones <sam.jones494@googlemail.com>
2019-09-23 16:23:18 +0100

    add types

--- changer/changer.py
+++ changer/changer.py
@@ -1,11 +1,15 @@
-def _calculate(change_amount, denominations=(200, 100, 50, 20, 10, 5, 2, 1)):
+from collections.abc import Iterable
+from typing import List
+
+
+def _calculate(change_amount: int, denominations=(200, 100, 50, 20, 10, 5, 2, 1)) -> Iterable:
     for coin in denominations:
         while change_amount >= coin:
             change_amount -= coin
             yield coin
 
 
-def change(change_amount):
+def change(change_amount: int) -> List[int]:
     return [coin for coin in _calculate(change_amount)]
 
 
--- coin_changer.py
+++ coin_changer.py
@@ -4,18 +4,18 @@ import json
 import changer.changer as changer
 
 
-def cli_main():
+def cli_main() -> None:
     if __invalid_args():
         raise Exception('Please provide a numeric argument')
     else:
         print(changer.change(int(sys.argv[1])))
 
 
-def __invalid_args():
+def __invalid_args() -> bool:
     return len(sys.argv) < 2 or sys.argv[1].isdigit() is False
 
 
-def lambda_main(event, context):
+def lambda_main(event, context) -> dict:
     change_amount = int(event['queryStringParameters']['change_amount'])
     response = {
         'statusCode': 200,
