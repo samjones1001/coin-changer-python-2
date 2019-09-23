af78672f815c778ca40e7d04fdbdca5ca1c7de6b
Sam Jones <sam.jones494@googlemail.com>
2019-09-23 11:32:06 +0100

    tidy alignment of terraform config files

--- lambda.tf
+++ lambda.tf
@@ -10,10 +10,10 @@ resource "aws_lambda_function" "coin-changer-2" {
   function_name = "CoinChanger2"
 
   s3_bucket = "sjones-s3-bucket-coin-changer-2"
-  s3_key = "lambda.zip"
+  s3_key =    "lambda.zip"
 
-  handler = "changer.handler"
-  runtime = "python3.6"
+  handler =   "changer.handler"
+  runtime =   "python3.6"
 
   role = "${aws_iam_role.lambda_exec.arn}"
 }
@@ -39,10 +39,10 @@ EOF
 }
 
 resource "aws_lambda_permission" "apigw" {
-  statement_id = "AllowAPIGatewayInvoke"
-  action = "lambda:InvokeFunction"
+  statement_id =  "AllowAPIGatewayInvoke"
+  action =        "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.coin-changer-2.function_name}"
-  principal = "apigateway.amazonaws.com"
+  principal =     "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.coin-changer-2-api.execution_arn}/*/*"
+  source_arn =    "${aws_api_gateway_rest_api.coin-changer-2-api.execution_arn}/*/*"
 }
